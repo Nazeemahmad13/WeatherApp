0e52a3b27bddc56bb113c9e9c0be958a37ba160c
Nazeem <nazeemahmad13@gmail.com>
2023-12-16 19:08:59 +0530

    Initial Progress "Started Creating GUI window for app and App Launcher"

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,35 @@
+import org.json.simple.JSONObject;
+
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+    private JSONObject weatherData;
+
+    public WeatherAppGui(){
+        // setup our gui and add a title
+        super("Weather App");
+
+        // configure gui to end the program's process once it has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // set the size of our gui (in pixels)
+        setSize(450, 650);
+
+        // load our gui at the center of the screen
+        setLocationRelativeTo(null);
+
+        // make our layout manager null to manually position our components within the gui
+        setLayout(null);
+
+        // prevent any resize of our gui
+        setResizable(false);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+
+
+    }
+
+}
